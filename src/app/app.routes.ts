c32cc80564ad2a74140a273fd2de58230784ef25
Martin Lipták <ing.martin.liptak@gmail.com>
2025-06-27 11:10:42 +0200

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { StandardComponent } from './pages/standard/standard.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { PrimengComponent } from './pages/primeng/primeng.component';
+import { MaterialComponent } from './pages/material/material.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: 'Home', pathMatch: 'full' }, // Toto je default
+    { path:'StandardComponent', component: StandardComponent },
+    { path:'GraphicsComponent', component: GraphicsComponent },
+    { path:'PrimengComponent', component: PrimengComponent },
+    { path:'MaterialComponent', component: MaterialComponent },
+    { path:'**', redirectTo: 'Home' }
+];
+
+export const appRouterModule=RouterModule.forRoot(routes);
+
---
+++ src/app/pages/primeng/primeng.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeng',
+  imports: [],
+  templateUrl: './primeng.component.html',
+  styleUrl: './primeng.component.css'
+})
+export class PrimengComponent {
+
+}
